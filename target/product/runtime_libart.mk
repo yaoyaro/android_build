a4ac1f732958a8f563303ef9d547d22cd843e9df
danya2271 <danya2271@yandex.ru>
2023-07-03 23:11:05 +0500

    Disable some logging
    Change-Id: Iacdaf2f0b8fcc3cdc67e0f61796fef73491de802

--- target/product/runtime_libart.mk
+++ target/product/runtime_libart.mk
@@ -145,8 +145,8 @@ PRODUCT_SYSTEM_PROPERTIES += \
 
 # Enable minidebuginfo generation unless overridden.
 PRODUCT_SYSTEM_PROPERTIES += \
-    dalvik.vm.minidebuginfo=true \
-    dalvik.vm.dex2oat-minidebuginfo=true
+    dalvik.vm.minidebuginfo=false \
+    dalvik.vm.dex2oat-minidebuginfo=false
 
 # Enable Madvising of the whole art, odex and vdex files to MADV_WILLNEED.
 # The size specified here is the size limit of how much of the file
