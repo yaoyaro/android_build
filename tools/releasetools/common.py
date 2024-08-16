6bad9119510ee870f6f141bb5e8028ac052bdd07
1582130940 <19632674+1582130940@users.noreply.github.com>
2024-08-16 14:18:24 +0800

    releasetools: Erase cache/dalvik-cache/resource-cahce/package_cache upon flashing
    * Erase cahce and dalvik-cache upon flashing
    * Erase resource-cahce upon flashing
    someone5678 <someone5678@users.noreply.github.com>
    
    * Erase package_cache upon flashing
    Ido Ben-Hur <idoybh2@gmail.com>
    
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -3613,6 +3613,18 @@ class BlockDifference(object):
                 new_data_name=new_data_name, code=code))
     script.AppendExtra(script.WordWrap(call))
 
+    call = ('delete_recursive("/data/cache");')
+    script.AppendExtra(script.WordWrap(call))
+
+    call = ('delete_recursive("/data/dalvik-cache");')
+    script.AppendExtra(script.WordWrap(call))
+
+    call = ('delete_recursive("/data/resource-cache");')
+    script.AppendExtra(script.WordWrap(call))
+
+    call = ('delete_recursive("/data/system/package_cache");')
+    script.AppendExtra(script.WordWrap(call))
+
   def _HashBlocks(self, source, ranges):  # pylint: disable=no-self-use
     data = source.ReadRangeSet(ranges)
     ctx = sha1()
