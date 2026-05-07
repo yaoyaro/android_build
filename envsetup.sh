74792dad2299e1d3ce51726ac8b96a50537d5691
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-05-07 12:52:38 +0800

    [script] removing swap toggle when using setupPerf
    Change-Id: I53bc7ea3125136c0242254f4f6243b4b9d115200

--- envsetup.sh
+++ envsetup.sh
@@ -2126,8 +2126,6 @@ function avbtool() {
 
 function setupPerf() {
     sudo sysctl -w vm.swappiness=1 >/dev/null
-    sudo swapoff -a
-    sudo swapon -a
     sudo sysctl -w vm.page-cluster=0 >/dev/null
 
     echo "setup build limits"
